f4071216f8f42bab95b2563b349491d83e2c34e4
ramazanmzeri <95154385+ramazanmzeri@users.noreply.github.com>
2021-11-28 06:14:39 +0300

    Update userbotjoin.py

--- VCPlayBot/modules/userbotjoin.py
+++ VCPlayBot/modules/userbotjoin.py
@@ -24,7 +24,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "VCPlayBot"
+        user.first_name = "vcplayeerrbot"
 
     try:
         await USER.join_chat(invitelink)
